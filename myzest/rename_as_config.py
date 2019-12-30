a49b6032e091baf465fdd9b0f12694921f8c9334
gareths3 <gareth.gs13@gmail.com>
2019-12-30 18:44:42 +0100

    config file
    Fill the config file to use and test app;

---
+++ myzest/rename_as_config.py
@@ -0,0 +1,13 @@
+"""
+App Private Data
+Set the 3 variables below then
+Rename this file 'config.py'
+"""
+# App Secret Key
+secret_key = "generate_your_secret"
+
+# Database connection for App
+mongo_uri = "insert_your_mongo_uri_here"
+
+# Database connection for tests
+test_mongo_uri = "insert_the_mongo_uri_for_tests"
